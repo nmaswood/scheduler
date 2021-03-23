fe3fc21cb12fca985a426031ebb1bff9cf49061d
Nasr Maswood <nasr@labelbox.com>
2021-03-23 11:21:35 -0400

    get fastify working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello");
+import fastify from "fastify";
+
+const server = fastify();
+const port = process.env.PORT ?? "8080";
+const host = process.env.HOST ?? "localhost";
+
+server.get("/", async (request, reply) => {
+  return { hello: "world!" };
+});
+
+server.listen(port, host, (err, address) => {
+  console.log(host);
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`server listening at ${address}`);
+});
