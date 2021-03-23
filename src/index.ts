3d70d8f3d3b7ad23f31800977b556101ab79bcfb
Nasr Maswood <nasr@labelbox.com>
2021-03-23 12:04:39 -0400

    update host

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import fastify from "fastify";
 
 const server = fastify();
 const port = process.env.PORT ?? "8080";
-const host = process.env.HOST ?? "localhost";
+const host = process.env.HOST ?? "0.0.0.0";
 
 server.get("/", async (request, reply) => {
   return { hello: "world!" };
